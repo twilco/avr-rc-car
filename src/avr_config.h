84d16a90b23578ed0bdc5b5beda2351a41e51cc9
Tyler Wilcock <tyler.l.wilcock@gmail.com>
2018-02-12 20:34:42 -0600

    Use 4MHz crystal instead of 16MHz crystal

--- src/avr_config.h
+++ src/avr_config.h
@@ -1,7 +1,7 @@
 #ifndef AVR_CONFIG_H_
 #define AVR_CONFIG_H_
 
-#define F_CPU (uint32_t) 16000000
+#define F_CPU (uint32_t) 4000000
 #define TIMER1_PRESCALER (uint8_t) 8
 #define TIMER0_PRESCALER (uint16_t) 8
 
